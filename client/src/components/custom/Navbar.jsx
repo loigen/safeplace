b17daa88795a15d01adbf6c1a32dffbafa165824
Dev-Loigen <loigen.lariosa@student.passerellesnumeriques.org>
2024-06-11 14:51:17 +0800

    loigen
    updates

--- client/src/components/custom/Navbar.jsx
+++ client/src/components/custom/Navbar.jsx
@@ -42,17 +42,18 @@ const Navpar = () => {
         <div className="menu-toggle md-hidden">
           <button className="menu-button" onClick={toggleMenu}>
             <svg
-              className="menu-icon"
-              fill="none"
-              stroke="currentColor"
+              width="24"
+              height="24"
               viewBox="0 0 24 24"
+              fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+                d="M3 6h18M3 12h18M3 18h18"
+                stroke="#000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
               />
             </svg>
           </button>
